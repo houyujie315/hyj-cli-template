e9b26b04c5873f397dd2c73b01b059f7720cdfe8
hyj <970174952@qq.com>
2021-01-31 10:44:53 +0800

    ant-admin模板

---
+++ src/components/GlobalFooter/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <global-footer class="footer custom-render">
+    <template v-slot:links>
+      <a href="/" target="_blank">{{ company }} Copyright &copy; {{ year }} 版权所有</a>
+    </template>
+    <template v-slot:copyright>
+
+    </template>
+  </global-footer>
+</template>
+
+<script>
+import { GlobalFooter } from '@ant-design-vue/pro-layout'
+
+export default {
+  name: 'ProGlobalFooter',
+  components: {
+    GlobalFooter
+  },
+  data () {
+    return {
+       company: process.env.VUE_APP_WEBMGR_COMPANY,
+       year: new Date().getFullYear()
+    }
+  }
+}
+</script>
